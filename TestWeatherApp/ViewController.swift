55ef8907939d3277d5aa3b84235c0fe88585d947
fdonsk0v <fedor@kolikosoft.com>
2026-03-30 16:58:38 +0300

    Add files project.

---
+++ TestWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWeatherApp
+//
+//  Created by Fedor Donskov on 30.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
